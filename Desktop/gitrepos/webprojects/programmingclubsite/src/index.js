adebb2ca31e5ffa8d6521f3a8b9ebb0152f94d6e
Brandon Ly <wowbob396@gmail.com>
2017-08-21 21:52:20 -0500

    Add files via upload

--- Desktop/gitrepos/webprojects/programmingclubsite/src/index.js
+++ Desktop/gitrepos/webprojects/programmingclubsite/src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function tick() {
+  const element = (
+    <div>
+      <h1>Normandale Programming Club</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
 
-ReactDOM.render(
-      <h1>Normandale Programming Club</h1>,
-      document.getElementById('root')    
-);
+setInterval(tick, 1000);
