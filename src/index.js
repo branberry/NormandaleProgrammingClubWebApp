86c1b87d660b50d2bbac2dbb528b27f71347df1b
Brandon <wowbob396@gmail.com>
2017-08-22 20:53:25 -0500

    added some cool stuff with a clock

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router'
 import './index.css';
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
 
-function tick() {
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
   const element = (
     <div>
       <h1>Normandale Programming Club</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <Clock />
     </div>
   );
+
   ReactDOM.render(
     element,
     document.getElementById('root')
   );
-}
-
-setInterval(tick, 1000);
